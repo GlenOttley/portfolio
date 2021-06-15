a90df36d6c3aadd0987e0f43694dd24566ae5e61
GlenOttley <glenottley@gmail.com>
2021-06-15 11:54:39 +0100

    added hidden contact form following simply web dev tutorial

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,24 @@
 import { useForm } from "react-hook-form";
 
 function Contact({ breakPoints }) {
+
+  async function onSubmit(data) {
+  	const response = await fetch("/submit", {
+  		method: "POST",
+  		headers: {
+  			"Content-Type": "application/json"
+  		},
+  		body: JSON.stringify({data})
+  	});
+
+  	if (response.ok) {
+  		alert("Thanks for getting in touch, I will get back to you shortly.");
+  		document.getElementById("contact-form").reset();
+  	} else {
+  		alert("Something went wrong, please try again.")
+  	}
+  }
+
   const { register, handleSubmit, formState: { errors } } = useForm();
 
 	return (
@@ -26,88 +44,94 @@ function Contact({ breakPoints }) {
 		  	<h2 className="form__heading">Contact Me</h2>
 
 			  <form 
-			  name="contact-form" 
+			  name="contact-form"
+			  id="contact-form" 
 			  method="POST"
-			  enctype="application/x-www-form-urlencoded"
-			  netlify
+			  data-netlify="true"
 			  >
+			  	<input 
+			  	type="hidden" 
+			  	name="form-name" 
+			  	value="contact-form" 
+			  	/>
+			  	
 			    <div className="form__group">
 
-		        <label
-		        className="form__label" 
-		        htmlFor="name">
-		        Name
-		        </label>
+			        <label
+			        className="form__label" 
+			        htmlFor="name">
+			        Name
+			        </label>
 
-		        <input {
-		        	...register("name", {
-		        		required: "This field is required" 
-		        	})}
-		        name="name"
-		        type="text" 
-		        className="form__field"     
-		        placeholder="Jane Appleseed"
-		        style={{ outline: errors.name ?  "2px solid var(--bright-red" : null }}
-		        />
+			        <input {
+			        	...register("name", {
+			        		required: "This field is required" 
+			        	})}
+			        name="name"
+			        type="text" 
+			        className="form__field"     
+			        placeholder="Jane Appleseed"
+			        style={{ outline: errors.name ?  "2px solid var(--bright-red" : null }}
+			        />
 
-		        {errors.name && 
-		        	<span className="form__error">{errors.name.message}</span>
-		        }
+			        {errors.name && 
+			        	<span className="form__error">{errors.name.message}</span>
+			        }
 
 			    </div>
 
 			    <div className="form__group">
 
-		        <label
-		        className="form__label" 
-		        htmlFor="exampleInputEmail1">
-		        Email Address
-		        </label>
+			        <label
+			        className="form__label" 
+			        htmlFor="exampleInputEmail1">
+			        Email Address
+			        </label>
 
-		        <input {
-		        	...register("email", {
-		        		required: "This field is required",
-		        		pattern: {
-		        			value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-		        			message: "Please use a valid email address"
-		        		}
-		        	})}
-		        name="email"
-		        type="text"
-		        className="form__field"
-		        placeholder="email@example.com"
-		        style={{ outline: errors.email ?  "2px solid var(--bright-red" : null }}
-		        />
+			        <input {
+			        	...register("email", {
+			        		required: "This field is required",
+			        		pattern: {
+			        			value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+			        			message: "Please use a valid email address"
+			        		}
+			        	})}
+			        name="email"
+			        type="text"
+			        className="form__field"
+			        placeholder="email@example.com"
+			        style={{ outline: errors.email ?  "2px solid var(--bright-red" : null }}
+			        />
 
-		        {errors.email && 
-		        	<span className="form__error">{errors.email.message}</span>
-		        }
+			        {errors.email && 
+			        	<span className="form__error">{errors.email.message}</span>
+			        }
 
 			    </div>
 
 			    <div className="form__group">
 
-		        <label
-		        className="form__label" 
-		        htmlFor="message">
-		        Message
-		        </label>
+			        <label
+			        className="form__label" 
+			        htmlFor="message">
+			        Message
+			        </label>
 
-		        <textarea {
-		        	...register("message", {
-		        		required: "This field is required" 
-		        	})}
-		        name="message"
-		        className="form__field" 
-		        rows="3"        
-		        placeholder="How can I help?"
-		        style={{ outline: errors.message ?  "2px solid var(--bright-red" : null }}
-		        >
-		        </textarea>
+			        <textarea {
+			        	...register("message", {
+			        		required: "This field is required" 
+			        	})}
+			        name="message"
+			        className="form__field" 
+			        rows="3"        
+			        placeholder="How can I help?"
+			        style={{ outline: errors.message ?  "2px solid var(--bright-red" : null }}
+			        >
+			        </textarea>
 
-		        {errors.message && 
-		        	<span className="form__error">{errors.message.message}</span>
-		        }
+			        {errors.message && 
+			        	<span className="form__error">{errors.message.message}</span>
+			        }
 
 			    </div>
 
