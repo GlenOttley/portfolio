503327a21d8a1105c0955e20f577dcb591a280f3
Glen Ottley <glenottley@gmail.com>
2022-07-10 10:27:40 -0400

    updated invoice app description

--- src/projectData.js
+++ src/projectData.js
@@ -2,14 +2,16 @@ const projectData = [
   {
     path: '/invoice-app',
     heading: 'Invoice App',
-    description: `This web app allows you to create and manage invoices online with ease`,
+    description: `This web app allows you to create and manage invoices online with ease.`,
     skills: 'State Management / Responsive Design',
     languages: 'Typescript / Node / React / Redux / MUI / MongoDB',
     website: 'https://invoice-application-mern.herokuapp.com/',
     code: 'https://github.com/GlenOttley/invoice-app',
     background: `For this project I decided to challenge myself on many levels, not only by building this out as a full stack application - 
     but also through the use of multiple new languages and tools. Both the front and back end are written in Typescript, with MongoDB being used for the database.
-    On the front end, Material UI is used to provide dynamic theming, React Hook Form for form validation, and Redux with Redux toolkit for state management.`,
+    On the front end, Material UI is used to provide dynamic theming, React Hook Form for form validation, and Redux with Redux toolkit for state management.
+    Please use the following credentials if you wish to see some pre-created invoices - email: johndoe@mail.com password: 1234. 
+    Alternatively, you can sign up and create your own! (dummy / fake email addresses are welcome).`,
     frontendMentor: true,
     prevProjectName: 'Crowdfund',
     prevProjectPath: '/crowdfund',
